21cd957eb3f0ef7d9fba9437695a19351b1f48a3
Erik Mortimer <mortimer.erik@gmail.com>
2019-12-28 12:40:53 -0600

    Reorganizing folders and files

--- src/chip.hpp
+++ src/chip.hpp
@@ -1,4 +1,5 @@
-#pragma once
+#ifndef chip_h
+#define chip_h
 
 #include <fstream>
 #include <cstring>
@@ -6,7 +7,7 @@
 #include <chrono>
 #include <random>
 #include <ctime>
-#include "SDL.h"
+#include <SDL2/SDL.h>
 
 const unsigned int START_ADDRESS = 0x200;
 const unsigned int FONTSET_START_ADDRESS = 0x50;
@@ -15,118 +16,21 @@ const unsigned int VIDEO_WIDTH = 64;
 const unsigned int VIDEO_HEIGHT = 32;
 
 class Chip8{
-    public:
-        uint8_t registers[16]{};
-        uint8_t memory[4096]{};
-        uint8_t fontset[FONTSET_SIZE] = {
-            0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
-            0x20, 0x60, 0x20, 0x20, 0x70, // 1
-            0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
-	        0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
-	        0x90, 0x90, 0xF0, 0x10, 0x10, // 4
-	        0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
-	        0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
-	        0xF0, 0x10, 0x20, 0x40, 0x40, // 7
-	        0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
-	        0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
-	        0xF0, 0x90, 0xF0, 0x90, 0x90, // A
-	        0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
-	        0xF0, 0x80, 0x80, 0x80, 0xF0, // C
-	        0xE0, 0x90, 0x90, 0x90, 0xE0, // D
-	        0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
-	        0xF0, 0x80, 0xF0, 0x80, 0x80  // F
-        };
-        uint16_t index{};
-        uint16_t pc{};
-        uint16_t stack[16]{};
-        uint8_t sp{};
-        uint8_t delayTimer{};
-        uint8_t soundTimer{};
-        uint8_t keypad[16]{};
+	public:
+		Chip8();
+		uint8_t keypad[16]{};
         uint32_t video[64 * 32]{};
-        uint16_t opcode;
 
-		void Table0(){
-			((*this).*(table0[opcode & 0x000Fu]))();
-		}
-		void Table8(){
-			((*this).*(table8[opcode & 0x000Fu]))();
-		}
-		void TableE(){
-			((*this).*(tableE[opcode & 0x000Fu]))();
-		}
-		void TableF(){
-			((*this).*(tableF[opcode & 0x00FFu]))();
-		}
-
-		void OP_NULL(){}
-
-		// Tables
-		typedef void (Chip8::*Chip8Func)();
-		Chip8Func table[0xF + 1]{&Chip8::OP_NULL};
-		Chip8Func table0[0xE + 1]{&Chip8::OP_NULL};
-		Chip8Func table8[0xE + 1]{&Chip8::OP_NULL};
-		Chip8Func tableE[0xE + 1]{&Chip8::OP_NULL};
-		Chip8Func tableF[0x65 + 1]{&Chip8::OP_NULL};
-
-        Chip8(){
-			// Init PC
-    		pc = START_ADDRESS;
-
-    		// Load the Font into memory
-    		for(unsigned int i = 0; i < FONTSET_SIZE; ++i){
-       			memory[FONTSET_START_ADDRESS + i] = fontset[i];
-    		}
-
-			// Set up function pointer table
-			table[0x0] = &Chip8::Table0;
-			table[0x1] = &Chip8::OP_1nnn;
-			table[0x2] = &Chip8::OP_2nnn;
-			table[0x3] = &Chip8::OP_3xkk;
-			table[0x4] = &Chip8::OP_4xkk;
-			table[0x5] = &Chip8::OP_5xy0;
-			table[0x6] = &Chip8::OP_6xkk;
-			table[0x7] = &Chip8::OP_7xkk;
-			table[0x8] = &Chip8::Table8;
-			table[0x9] = &Chip8::OP_9xy0;
-			table[0xA] = &Chip8::OP_Annn;
-			table[0xB] = &Chip8::OP_Bnnn;
-			table[0xC] = &Chip8::OP_Cxkk;
-			table[0xD] = &Chip8::OP_Dxyn;
-			table[0xE] = &Chip8::TableE;
-			table[0xF] = &Chip8::TableF;
-
-			table0[0x0] = &Chip8::OP_00E0;
-			table0[0xE] = &Chip8::OP_00EE;
-
-			table8[0x0] = &Chip8::OP_8xy0;
-			table8[0x1] = &Chip8::OP_8xy1;
-			table8[0x2] = &Chip8::OP_8xy2;
-			table8[0x3] = &Chip8::OP_8xy3;
-			table8[0x4] = &Chip8::OP_8xy4;
-			table8[0x5] = &Chip8::OP_8xy5;
-			table8[0x6] = &Chip8::OP_8xy6;
-			table8[0x7] = &Chip8::OP_8xy7;
-			table8[0xE] = &Chip8::OP_8xyE;
-
-			tableE[0x1] = &Chip8::OP_ExA1;
-			tableE[0xE] = &Chip8::OP_Ex9E;
-
-			tableF[0x07] = &Chip8::OP_Fx07;
-			tableF[0x0A] = &Chip8::OP_Fx0A;
-			tableF[0x15] = &Chip8::OP_Fx15;
-			tableF[0x18] = &Chip8::OP_Fx18;
-			tableF[0x1E] = &Chip8::OP_Fx1E;
-			tableF[0x29] = &Chip8::OP_Fx29;
-			tableF[0x33] = &Chip8::OP_Fx33;
-			tableF[0x55] = &Chip8::OP_Fx55;
-			tableF[0x65] = &Chip8::OP_Fx65;
-
-        }
-        void LoadROM(char const* filename);
+		void LoadROM(char const* filename);
 		void Cycle();
-
+	private:
+		void Table0();
+		void Table8();
+		void TableE();
+		void TableF();
+		
 		// Instructions
+		void OP_NULL();
 		void OP_00E0(); // CLS: Clear the display
 		void OP_00EE(); // RET: Return from a subroutine
 		void OP_1nnn(); // 1nnn: JP addr
@@ -162,9 +66,26 @@ class Chip8{
 		void OP_Fx55(); // Fx55: LD [I], Vx
 		void OP_Fx65(); // Fx65: LD Vx, [I]
 
-		//Seed rng
-		//srand(time(NULL));
-};
+		uint8_t registers[16]{};
+        uint8_t memory[4096]{};
+        
+        uint16_t index{};
+        uint16_t pc{};
+        uint16_t stack[16]{};
+        uint8_t sp{};
+        uint8_t delayTimer{};
+        uint8_t soundTimer{};
+        uint16_t opcode{};
+		
+		// Tables
+		typedef void (Chip8::*Chip8Func)();
+		Chip8Func table[0xF + 1]{&Chip8::OP_NULL};
+		Chip8Func table0[0xE + 1]{&Chip8::OP_NULL};
+		Chip8Func table8[0xE + 1]{&Chip8::OP_NULL};
+		Chip8Func tableE[0xE + 1]{&Chip8::OP_NULL};
+		Chip8Func tableF[0x65 + 1]{&Chip8::OP_NULL};
+
+    };
 
 class Platform{
 	private:
@@ -339,3 +260,5 @@ class Platform{
 			return quit;
 		}
 };
+
+#endif
